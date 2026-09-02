4e98a7c520a9faa0fa90dadf92fdbed8988aa970
Caio Bandeira <caiofelipeb15@gmail.com>
2026-09-02 16:10:36 -0300

    Upload do Spring Boot Default

---
+++ src/main/java/edu/sistema/sigedef/SigedefApplication.java
@@ -0,0 +1,13 @@
+package edu.sistema.sigedef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigedefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigedefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/sistema/sigedef/SigedefApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sistema.sigedef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigedefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
